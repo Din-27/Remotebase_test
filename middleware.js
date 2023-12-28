15c174af47f6430e89183b52857f1f2c9f19e18e
Admin <admin.push@hackerrank.workspace>
2023-12-28 08:21:12 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- middleware.js
+++ middleware.js
@@ -1,5 +1,12 @@
 const VALID_KEYS_PATH = __dirname + '/valid-keys.txt';
+const shortid = require('shortid');
+
 
 module.exports = function (req, res, next) {
-  console.log(req)
+  const verify = shortid.isValid()
+  if(verify){
+    return next()
+  }else{
+    return res.status(401)
+  }
 };
